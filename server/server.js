6c672be86d83a7bfdbe3d8ebb75aa03696902413
ivan3123708 <ivan3123708@gmail.com>
2018-03-13 22:04:42 +0100

    Added GET /todos route and tests

--- server/server.js
+++ server/server.js
@@ -8,6 +8,14 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({ todos })
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
 app.post('/todos', (req, res) => {
   const todo = new Todo({
     text: req.body.text,
